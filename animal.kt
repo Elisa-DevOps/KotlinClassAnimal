c3c7b3c0ccca1effa4d318fb702615eaa18c2bde
Elisa <elisa-devops@users.noreply.github.com>
2024-10-13 15:24:12 -0500

    showed how declarative class type variable vastly limits scope of calling into those classes

--- animal.kt
+++ animal.kt
@@ -16,19 +16,28 @@ open class Animal(val name: String) {
 
 class Cat: Animal {
     constructor(name: String): super(name) {
-        println("inside secondary constructor")
+        println("inside secondary constructor of class cat") //auto generates on initialization
     }
     
     override fun makeSound(): String {
         return "meow"
     }
+
+    fun playYarn(): String {
+        return "played with yarn"
+    }
 }
 
 fun main() {
-    val dog = Animal("Spot")
+    val dog : Animal = Animal("Spot")
     println(dog.name)       //property 'name' inside constructor that can be accessed
     println("${dog.name} has ${dog.legCount()} legs")
     
     val cat = Cat("Kitty")
     println(cat.makeSound())
+    println("${cat.name} has ${cat.legCount()} legs")
+    println(cat.playYarn())
+
+    val cat2 : Animal = Cat("Kitty2")
+    //println(cat2.playYarn())    //Error intended to show cat2 cannot play yarn, class variable type is restricted to only what class Animal can access
 }
